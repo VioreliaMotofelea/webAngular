c1c9e48b2c8f032f13cfc314e54a3a0d3bcbae14
VioreliaMotofelea <viorelia.motofelea123@gmail.com>
2025-05-23 14:11:48 +0300

    Re-add Angular UI as normal folder and update project files

--- api/get_destinations.php
+++ api/get_destinations.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
